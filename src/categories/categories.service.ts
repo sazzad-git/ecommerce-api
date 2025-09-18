8f374b5fb44aa18b9c42613a00b17ba7d5b180be
sazzad-git <sazzadurrahman000@gmail.com>
2025-09-18 16:58:07 +0600

    last update

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CategoryEntity } from './entities/category.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CategoriesService {
+  constructor(
+    @InjectRepository(CategoryEntity)
+    private readonly categoryRepository: Repository<CategoryEntity>,
+  ) {}
   create(createCategoryDto: CreateCategoryDto) {
     return 'This action adds a new category';
   }
--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,1 +1,11 @@
-export class CreateCategoryDto {}
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateCategoryDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+}
